b536a962a7d1dff4e364afac0b3d4ca5da5cfb42
Umebo <igor.niemiec@wichry.pl>
2017-09-02 01:14:37 +0200

    Pobranie odpowiedzi z serwera Sprawdzenie obecności w bazie ew. zapisanie do bazy

---
+++ Program_1.0.py
@@ -0,0 +1,22 @@
+import wikipedia
+import pprint
+from pymongo import MongoClient
+
+# Podlaczenie do lokalnej bazy danych
+client = MongoClient()
+db = client.countries
+
+def saving_country(country_input):
+    country = wikipedia.WikipediaPage(country_input).summary
+    country_data = { 'name': country_name, 'Summary': country }
+    mongo_country = db.countries.insert_one(country_data)
+
+country_input = input(' Select country: ')
+country_name = wikipedia.WikipediaPage(country_input).title
+
+if db.countries.find({'name': country_name}):
+    saving_country(country_input)
+else:
+    print('You have that one already!')
+    pprint.pprint(db.countries.find_one({'name': country_name}))
+
