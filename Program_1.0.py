ec738cb614698d813a4cc35f82eaf102ddaed797
Unknown <igor.niemiec@wichry.pl>
2017-09-02 11:13:38 +0200

    Działająca wersja bazowa

--- Program_1.0.py
+++ Program_1.0.py
@@ -8,15 +8,15 @@ db = client.countries
 
 def saving_country(country_input):
     country = wikipedia.WikipediaPage(country_input).summary
-    country_data = { 'name': country_name, 'Summary': country }
-    mongo_country = db.countries.insert_one(country_data)
+    country_data = {'name': country_name, 'Summary': country}
+    db.countries.insert_one(country_data)
 
 country_input = input(' Select country: ')
 country_name = wikipedia.WikipediaPage(country_input).title
 
-if db.countries.find_one({'name': country_name}):
+
+if db.countries.find({'name': country_name}).count() == 0:
     saving_country(country_input)
 else:
     print('You have that one already!')
     pprint.pprint(db.countries.find_one({'name': country_name}))
-    
