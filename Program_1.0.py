e834953f532f48dea044fc43c5ff4b5aedf759e7
Unknown <igor.niemiec@wichry.pl>
2017-09-04 15:44:02 +0200

    Porownywanie obrazow z pewnym marginesem

--- Program_1.0.py
+++ Program_1.0.py
@@ -1,5 +1,6 @@
 import wikipedia
 import pprint
+import imgcompare
 from pymongo import MongoClient
 
 # Podlaczenie do lokalnej bazy danych
@@ -11,6 +12,7 @@ def saving_country(country_input):
     country_data = {'name': country_name, 'Summary': country}
     db.countries.insert_one(country_data)
 
+
 country_input = input(' Select country: ')
 country_name = wikipedia.WikipediaPage(country_input).title
 
@@ -20,3 +22,8 @@ if db.countries.find({'name': country_name}).count() == 0:
 else:
     print('You have that one already!')
     pprint.pprint(db.countries.find_one({'name': country_name}))
+
+
+same = imgcompare.is_equal('/home/igor/Documents/Python/GitKraken/Wikipedia_project/flag.png',
+                          ('/home/igor/Documents/Python/GitKraken/Wikipedia_project/Flags/' + country_name + '.png'),
+                           tolerance = 2.5)
