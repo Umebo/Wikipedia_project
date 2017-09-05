7b0346ef4cd4db795811d385066b9f0cc1f5fbff
Unknown <igor.niemiec@wichry.pl>
2017-09-05 15:56:22 +0200

    Prownanie wszystkich obrazow, wybranie najmniejszej roznicy. Zmienione na funkcje.

--- Program_1.0.py
+++ Program_1.0.py
@@ -10,7 +10,7 @@ client = MongoClient()
 db = client.countries
 
 
-def SavingCountry(country_input):
+def saving_country_to_db(country_input):
     country = wikipedia.WikipediaPage(country_input).summary
     country_data = {'name': country_name, 'Summary': country}
     db.countries.insert_one(country_data)
@@ -21,25 +21,30 @@ country_name = wikipedia.WikipediaPage(country_input).title
 
 
 if db.countries.find({'name': country_name}).count() == 0:
-    SavingCountry(country_input)
+    saving_country_to_db(country_input)
 else:
     print('You have that one already!')
     pprint.pprint(db.countries.find_one({'name': country_name}))
 
+###################################################
 
-flag = Image.open('/home/igor/Documents/Python/GitKraken/Wikipedia_project/flag2.png').convert('RGB')
+flag = Image.open('flag.png').convert('RGB')
 
-def FlagName(path):
+def flag_name(path):
     flag_name = ''
     for letter in path[len(path)-5::-1]:
         if letter != '/':
             flag_name = flag_name + letter
         else:
-            break
-    return flag_name[::-1]
+            return flag_name[::-1]
 
+def comparing_flags(input_flag):
+    difference = 30.0
+    for file in glob.glob('Flags/*.png'):
+        test_flag = Image.open(file).resize(input_flag.size).convert('RGB')
+        percentage = imgcompare.image_diff_percent(test_flag, input_flag)
+        if percentage < difference:
+            result = (flag_name(file))
+    return result
 
-for file in glob.glob('/home/igor/Documents/Python/GitKraken/Wikipedia_project/Flags/*.png'):
-    test_flag = Image.open(file).resize(flag.size).convert('RGB')
-    if imgcompare.is_equal(flag, test_flag, tolerance = 20.0):
-        print(FlagName(file))
+print(comparing_flags(flag))
